41f08fbe431e57a8f2d59e6de52dae067a44f163
COELHO DO AMARAL <soraya.coelho@t-systems.com>
2026-01-10 12:25:36 +0100

    Add Local Storage

--- src/js/form.js
+++ src/js/form.js
@@ -2,6 +2,9 @@ console.log('>> Ready form:)');
 
 const inputs = document.querySelectorAll(".campo");
 const parrafo = document.getElementById("form_container-result");
+const form = document.querySelector(".form_container-sign");
+
+
 
 inputs.forEach(input => {
 input.addEventListener('input', manejarInput);
@@ -14,3 +17,30 @@ function manejarInput(event) {
   
 }
 
+//Guardar datos en Local Storage
+
+function loadLocalStorage() {
+  const savedData = JSON.parse(localStorage.getItem('formData'));
+
+  if (savedData) {
+    form.elements.firstName.value = savedData.firstName || '';
+    form.elements.proposal.value = savedData.proposal || '';
+    form.elements.city.value = savedData.city || '';
+    form.elements.phone.value = savedData.phone || '';
+    form.elements.email.value = savedData.email || '';
+  }
+}
+
+loadLocalStorage();
+
+form.addEventListener('input', () => {
+  const formData = {
+    firstName: form.elements.firstName.value,
+    proposal: form.elements.proposal.value,
+    city: form.elements.city.value,
+    phone: form.elements.phone.value,
+    email: form.elements.email.value,
+  };
+
+  localStorage.setItem('formData', JSON.stringify(formData));
+});
